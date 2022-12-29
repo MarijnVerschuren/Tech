0a01f56fe23a1b864972fa2c42fe08eee6776964
MarijnVerschuren <marijnverschuren3@gmail.com>
2022-12-29 01:52:02 +0100

    Finished Chapter 5

--- SerialPlotter/SerialPlotter.ino
+++ SerialPlotter/SerialPlotter.ino
@@ -1,13 +1,13 @@
-#define AS5600_DEG_CONV 8.7890625e-2	/* 360/4096 */
-#define AS5600_RAD_CONV 1.5339808e-3	/* 2pi/4096 */
-
 void setup() {
   Serial.begin(115200);
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, LOW);
+  pinMode(A2, OUTPUT);
+  digitalWrite(A2, HIGH);
 }
 
-char msg_buffer[30];  // text (14 chars) + raw (4 chars) + deg (6 chars) + rad (6 chars)
 void loop() { 
-  uint16_t raw = analogRead(A0) << 2;  // analog read only provides 10 bits of precision...
-  sprintf(msg_buffer, "raw:%4u,deg:%4.1f,rad:%4.4f", raw, raw * AS5600_DEG_CONV, raw * AS5600_RAD_CONV);
-  Serial.print(msg_buffer);
+  uint16_t raw = analogRead(A1);
+  Serial.print("\nraw:");       Serial.print(raw);
+  Serial.print(",percentage:"); Serial.print((double)raw / 10.24);
 }
