0030d88db913562f919b2916594b8324c5b4288c
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-01-10 20:05:58 +0100

    Added rider C# project

---
+++ SEM_2/Class_Diagram/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Class_Diagram {
+    internal class Program {
+        public static int Main(string[] args) {
+            System.Console.WriteLine()
+            return 0;
+        }
+    }
+}
