bafb02d447024904abdf69fa479deb2f5138c5e2
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-02-09 14:33:32 +0100

    Removed older semesters

---
+++ embedded/gitlab.sh
@@ -0,0 +1,1 @@
+xdg-open https://git.fhict.nl/technology/t-sem3-db/-/tree/master/embedded-systems
---
+++ project/design/gitlab.sh
@@ -0,0 +1,1 @@
+xdg-open https://git.fhict.nl/technology/t-sem3-cb/-/tree/master/projects/Design%20Project
