d86ebd7f04315003e5038a23efd996c0db637fac
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-06-28 14:17:36 +0200

    Graphs working

--- software/algo/graphs_trees/ass6/2/ass6_2.cpp
+++ software/algo/graphs_trees/ass6/2/ass6_2.cpp
@@ -1,34 +1,46 @@
 #include <iostream>
-#include <string.h>
-
-#include "graph.hpp"
-
+#include <vector>
+#include <stdint.h>
+#include <list>
 
 using namespace std;
+
+struct queue_item {
+	uint64_t node;
+	uint64_t level;
+};
 
 
 int main(){
 	uint64_t t, n, m, x, y;
-	uint64_t route_count;
-	Graph<uint64_t>* graphs[100000];  // 10^5 max nodes
 
-    cin >> t;
-    for(uint64_t i = 0; i < t; i++) {
-		cin >> n >> m;
-		for (uint64_t j = 0; j < m + 1; j++) { graphs[j] = new Graph<uint64_t>(j + 1); }
+	cin >> t;  // 't' tests
+	for(uint64_t i = 0; i < t; i++) {
+		cin >> n >> m;  // 'n' nodes, 'm' inputs
+		vector<vector<uint64_t>>	neighbors(n);
+		vector<bool>				visited(n);
+		list<queue_item>			queue;
+		for (uint64_t j = 0; j < n; j++) { visited[j] = false; }
 		for (uint64_t j = 0; j < m; j++) {
-			cin >> x >> y;
-			graphs[x - 1]->add(graphs[y - 1]);
+			cin >> x >> y;  // 'x' connected to 'y'
+			neighbors[x-1].push_back(y-1);
+			neighbors[y-1].push_back(x-1);
 		}
-		Path<uint64_t>* path = BFS(graphs[0], n);
-		if (!path) { cout << "not found\n"; continue; }
-		route_count = path->step_count();
-		cout << route_count << " ";
-		print(path);
-		delete graphs[0];
-    }
+		// 'visit' first node
+		queue.push_back({0, 0});
+		visited[0] = true;
+		// BFS traverse
+		while (!queue.empty()) {
+			queue_item current = queue.front();
+			if (current.node == (n-1)) { std::cout << current.level << "\n"; }
+			queue.pop_front();
+			for (uint64_t neighbor : neighbors[current.node]) {
+				if (visited[neighbor]) { continue; }
+				queue.push_back({neighbor, current.level + 1});
+				visited[neighbor] = true;
+			}
+		}
+	}
 
-    return 0;
+	return 0;
 }
-// FKIN seg fault on depth func
-// TODO: fix cyclic references
--- software/algo/graphs_trees/project/src/main.cpp
+++ software/algo/graphs_trees/project/src/main.cpp
@@ -1,11 +1,44 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 #include <stdint.h>
-#include <stdlib.h>
+#include <list>
 
 #include "graph.hpp"
+using namespace std;
 
 
 int main() {
+	uint64_t t, n, m, x, y;
+
+	cin >> t;  // 't' tests
+	for(uint64_t i = 0; i < t; i++) {
+		cin >> n >> m;  // 'n' nodes, 'm' inputs
+		vector<vector<uint64_t>>	neighbors(n);
+		vector<bool>				visited(n);
+		list<uint64_t>	queue;
+		for (uint64_t j = 0; j < n; j++) { visited[j] = false; }
+		for (uint64_t j = 0; j < m; j++) {
+			cin >> x >> y;  // 'x' connected to 'y'
+			neighbors[x-1].push_back(y-1);
+			neighbors[y-1].push_back(x-1);
+		}
+		// 'visit' first node
+		queue.push_back(0);
+		visited[0] = true;
+		// BFS traverse
+		while (!queue.empty()) {
+			uint64_t current = queue.front();
+			queue.pop_front();
+			std::cout << "visit: " << current << "\n";
+			for (uint64_t neighbor : neighbors[current]) {
+				queue.push_back(neighbor);
+				visited[neighbor] = true;
+			}
+		}
+	}
+
+	return 0;
+	/*
 	Graph<uint64_t>* graph = new Graph<uint64_t>(1);
 	Graph<uint64_t> *a, *b, *c, *d;
 	Path<uint64_t>* path;
@@ -27,4 +60,5 @@ int main() {
 	path = BFS<uint64_t>(graph, 3ul); print(path);
 
 	return 0;
+	*/
 }
