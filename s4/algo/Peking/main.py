a5ca8bf3d34efd761c7d9ed2cfebe862c5d10736
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-11-02 23:34:28 +0100

    refactor + planning

--- s4/algo/Peking/main.py
+++ s4/algo/Peking/main.py
@@ -1,122 +1,129 @@
 import json
-from functools import cache
 import sys
+from typing import *
 
 import pandas as pd
 
 
 class InputError(Exception):
-    """problems with the command line input"""
+	"""problems with the command line input"""
 
 
 class Node:
-    def __init__(self, num: int, neighbors: list[tuple["Node", int]], critical=False):
-        self.num = num
-        self.critical = critical
-        self.neighbors = neighbors  # To: cost
+	def __init__(self, num: int, critical=False):
+		self.num = num
+		self.critical = critical
+		self.neighbors: List[Tuple["Node", int]] = []
 
-    def __str__(self) -> str:
-        return f"{'*' if self.critical else ''}({self.num})"
+	def __str__(self) -> str:
+		return f"{'*' if self.critical else ''}({self.num})"
 
-    def __eq__(self, other: int) -> bool:
-        return self.num == other
+	def __eq__(self, other: int) -> bool:
+		return self.num == other
 
 
 class Player:
-    def __init__(self, budget: int, start: Node):
-        self.budget = budget
-        self.node = start
+	def __init__(self, budget: int, start: Node):
+		self.budget = budget
+		self.node = start
 
-    def __str__(self) -> str:
-        return f"at {self.node} with budget of {self.budget}"
-
-
-def GetEntirePath(fromNodeNum: int, shortestPaths: dict[int, tuple[Node, int]]) -> list[Node]:
-    if fromNodeNum not in shortestPaths:
-        return []
-
-    if shortestPaths[fromNodeNum][0] == fromNodeNum:  # At start node
-        return [GetNode(nodes, fromNodeNum)]
-
-    if not shortestPaths[fromNodeNum][0]:
-        return []
-
-    if path := GetEntirePath(shortestPaths[fromNodeNum][0].num, shortestPaths):
-        return path + [GetNode(nodes, fromNodeNum)]
-
-    return []
+	def __str__(self) -> str:
+		return f"at {self.node} with budget of {self.budget}"
 
 
-def GetNode(nodes: list[Node], num: int) -> Node:
-    for node in nodes:
-        if node.num == num:
-            return node
-    else:
-        raise Exception(f"Node not found, num: {num}")
+def get_path(from_node: int, shortest_paths: Dict[int, Tuple[Node, int]]) -> List[Node]:
+	if from_node not in shortest_paths:
+		return []
 
+	if shortest_paths[from_node][0] == from_node:  # At start node
+		return [get_node(nodes, from_node)]
+
+	if not shortest_paths[from_node][0]:
+		return []
+
+	if path := get_path(shortest_paths[from_node][0].num, shortest_paths):
+		return path + [get_node(nodes, from_node)]
+
+	return []
+
+
+def get_node(nodes: List[Node], num: int) -> Node: return nodes[nodes.index(num)]
 
 # Dijkstra
 #   Critical locations
 #   Budget
-def GetShortestFromNode(nodes: list[Node], start: Node) -> dict[int, tuple[Node, int]]:  # Result: O((M+1)N)
-    shortestPaths = {node.num: (None, float('inf')) for node in nodes}  # Node: (from, cost). O(N)
-    shortestPaths[start.num] = (start.num, 0)
+def node_paths(nodes: List[Node], start: Node) -> Dict[int, Tuple[Node, int]]:  # Result: O((M+1)N)
+	shortest_paths = {node.num: (None, float('inf')) for node in nodes}  # Node: (from, cost). O(N)
+	shortest_paths[start.num] = (start.num, 0)
 
-    queue = [start]
+	queue = [start]
 
-    while queue:  # Result: O(MN)
-        currentNode = queue.pop(0)  # O(N), since all elements have to be shifted.
+	while queue:  # Result: O(MN)
+		currentNode = queue.pop(0)  # O(N), since all elements have to be shifted.
 
-        for neighborNum, cost in currentNode.neighbors:  # O(M)
-            newDist = shortestPaths[currentNode.num][1] + cost
+		for neighbor, cost in currentNode.neighbors:  # O(M)
+			newDist = shortest_paths[currentNode.num][1] + cost
 
-            if newDist < shortestPaths[neighborNum][1]:
-                shortestPaths[neighborNum] = (currentNode, newDist)
-                queue.append(GetNode(nodes, neighborNum))
+			if newDist < shortest_paths[neighbor.num][1]:
+				shortest_paths[neighbor.num] = (currentNode, newDist)
+				queue.append(neighbor)
 
-    return shortestPaths
-
-
-def DijkstraPlus(nodes: list[Node]) -> pd.DataFrame:  # Result: O((M+2)N)
-    results = pd.DataFrame(columns=['From', 'To', 'Path', 'Cost'])
-
-    for start in nodes:
-        shortestPaths = GetShortestFromNode(nodes, start)  # O((M+1)N)
-
-        for node, (fromNode, cost) in shortestPaths.items():  # O(N)
-            results.loc[len(results.index)] = [start.num, node, GetEntirePath(node, shortestPaths), cost]
-
-    return results
+	return shortest_paths
 
 
-def GetNeighbors(roads: dict[str, int], node: int) -> list[tuple[str, int]]:
-    return [(to, price) for start, to, price in zip(roads["a"], roads["b"], roads["price"]) if node == start] + \
-           [(start, price) for start, to, price in zip(roads["a"], roads["b"], roads["price"]) if node == to]
+def dijkstra(nodes: List[Node]) -> pd.DataFrame:  # Result: O((M+2)N)
+	results = pd.DataFrame(columns=['From', 'To', 'Path', 'Cost'])
+
+	for start in nodes:
+		shortest_paths = node_paths(nodes, start)  # O((M+1)N)
+
+		for node, (fromNode, cost) in shortest_paths.items():  # O(N)
+			results.loc[len(results.index)] = [start.num, node, get_path(node, shortest_paths), cost]
+
+	return results
 
 
 if __name__ == '__main__':
-    if "-file" not in sys.argv:
-        raise InputError("input file not provided")
-    if not ("-white" or "-black" in sys.argv):
-        raise InputError("player not provided")
-    file = sys.argv[sys.argv.index("-file") + 1]
+	if "-file" not in sys.argv:
+		raise InputError("input file not provided")
+	if not ("-white" or "-black" in sys.argv):
+		raise InputError("player not provided")
+	file = sys.argv[sys.argv.index("-file") + 1]
 
-    try:
-        with open(file, "r") as file:
-            data = json.load(file)
+	try:
+		with open(file, "r") as file:
+			data = json.load(file)
 
-        locations, roads, start_node, budget = data.values()
-        nodes = [Node(n,
-                      GetNeighbors(roads, n),
-                      n in locations["critical"]) for n in list(set(roads["a"] + roads["b"]))]
-        # edges = [(a, roads["b"][i], roads["price"][i]) for i, a in enumerate(roads["a"])]
-    except:
-        raise InputError("input file has the wrong format")
+		locations, roads, start_node, budget = data.values()
+		nodes = [Node(n, n in locations["critical"]) for n in list(set(roads["a"] + roads["b"]))]
+		for start, end, price in zip(roads["a"], roads["b"], roads["price"]):
+			start = nodes[nodes.index(start)]
+			end = nodes[nodes.index(end)]
+			start.neighbors.append((end, price))
+			end.neighbors.append((start, price))
+	except:
+		raise InputError("input file has the wrong format")
 
-    white = Player(budget, start_node)
-    black = Player(budget, start_node)
-    you = white if "-white" in sys.argv else black
-    print(f"white {white}")
-    print(f"white {black}")
+	white = Player(budget, start_node)
+	black = Player(budget, start_node)
+	you = white if "-white" in sys.argv else black
+	print(f"white {white}")
+	print(f"white {black}")
 
-    print(DijkstraPlus(nodes))  # O((M+2)N)
+	print(dijkstra(nodes))
+
+	# if white:
+	#	if white can afford to reach the end:
+	#		head towards the end
+	#	if not on c_node or black is nearing next c_node in path:  TODO: black is nearing c_node if white can just about win the race towards it
+	#		head towards the c_node in path
+	#	else: wait on c_node for budget
+
+	# if black:
+	#	if black can afford to reach the end:
+	#		head towards the end
+	#	if not on c_node and black can reach before white:	TODO: white is nearing c_node if black can just about win the race towards it
+	#		head towards the c_node in path
+	#	elif not on c_node:
+	#		look for the fastes alternative path towards the next critical node in the fastest path
+	#		if this does not exist walk the best path from current node to end.
